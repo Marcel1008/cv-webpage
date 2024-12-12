28fae5b35c2362c0b8e2ab74a2bc9922830dc312
Marcel1008 <marcel.busniess@gmail.com>
2024-12-12 08:07:54 +0000

    Test

---
+++ test.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+st.title("my name",anchor=False)
+st.header("ente",anchor=False,divider=True)
+st.subheader("hallo",anchor=False,divider=True)
+st.write("webseite von marcel")
+
+st.title("how i feel",anchor=False)
+st.header("kalk",anchor=False,divider=True)
+st.subheader("stimme",anchor=False,divider=True)
+st.write("Heute geht es mir gut")
+
+st.title("name",anchor=False)
+st.header("kleiner",anchor=False,divider=True)
+st.subheader("noch kleiner",anchor=False,divider=True)
+st.write("ich hiesse marcel")
+
+st.title("my nachnsme",anchor=False)
+st.header("wasserziechen",anchor=False,divider=True)
+st.subheader("scghwimmen",anchor=False,divider=True)
+st.write("mein nachname ist aria")
+
+st.title("hobby",anchor=False)
+st.header("tastatur",anchor=False,divider=True)
+st.subheader("stern",anchor=False,divider=True)
+st.write("ich spiele gerne billiard")
+
